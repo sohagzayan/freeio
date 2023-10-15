7029c161ad849a74cd92c87f15064a59870b438e
sohagzayan <gdsohag360@gmail.com>
2023-10-14 22:47:25 -0700

    another code comand

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,5 @@
 "use client"
-import AuthSocial from "@/components/common/AuthSocial/AuthSocial"
-import ButtonLarge from "@/components/common/Button/ButtonLarge"
-import Divider from "@/components/common/Divider/Divider"
 import Footer from "@/components/common/Footer/Footer"
-import AuthField from "@/components/common/InputField/AuthField"
-import { Checkbox } from "@material-tailwind/react"
-import Link from "next/link"
 
 const page = () => {
     const inputHandle = () => {
@@ -18,11 +12,11 @@ const page = () => {
         <div className="bg-skin-peach_shade">
             <div className="container mx-auto md:px-10 px-4 py-20 lg:w-[55%] md:w-[70%] w-[100%]">
                 <div className="">
-                    {/* <div className="text-center mb-10">
+                    <div className="text-center mb-10">
                         <h3 className="text-[32px] font-[700] text-skin-dark_gray_shade font-primary mb-2">Log In</h3>
                         <p className="text-[14px] font-[400] text-skin-dark_gray_shade font-primary">Give your visitor a smooth online experience with a solid UX design</p>
-                    </div> */}
-                    <div className="bg-skin-white_shade rounded-lg p-8">
+                    </div>
+                    {/* <div className="bg-skin-white_shade rounded-lg p-8">
                         <div>
                             <h3 className="text-[20px] font-[500] text-skin-dark_gray_shade font-primary mb-2">We're glad to see you again!
                             </h3>
@@ -43,7 +37,7 @@ const page = () => {
                         </form>
                         <Divider />
                         <AuthSocial />
-                    </div>
+                    </div> */}
                 </div>
             </div>
             <Footer />
