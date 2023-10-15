ba6843e134620f4bd5e68ec46bc5151d6a85a68c
sohagzayan <gdsohag360@gmail.com>
2023-10-14 23:02:47 -0700

    dd

--- src/components/common/AuthSocial/AuthSocial.tsx
+++ src/components/common/AuthSocial/AuthSocial.tsx
@@ -1,16 +1,15 @@
-import { Button } from "@material-tailwind/react"
 
 const AuthSocial = () => {
     return (
         <div className="mt-5">
             <div className="grid grid-cols-2 gap-4  ">
-                <Button
+                {/* <Button
                     size="sm"
                     variant="outlined"
                     color="blue-gray"
                     className="flex items-center gap-3"
                 >
-                    {/* <Image src={GoogleIcon} alt="metamask" className="h-6 w-6" /> */}
+                    <Image src={GoogleIcon} alt="metamask" className="h-6 w-6" />
                     Sign in with Twitter
 
                 </Button>
@@ -22,9 +21,9 @@ const AuthSocial = () => {
                 >
                     Sign in with Twitter
                     <span className="absolute right-0 grid h-full w-12 place-items-center bg-light-blue-600 transition-colors group-hover:bg-light-blue-700">
-                        {/* <Image src={FacebookIcon} alt="metamask" className="h-6 w-6" /> */}
+                        <Image src={FacebookIcon} alt="metamask" className="h-6 w-6" />
                     </span>
-                </Button>
+                </Button> */}
             </div>
         </div>
     )
