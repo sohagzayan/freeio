a9ef2062a4cbd5e212e3cd59526a31df23723388
sohagzayan <gdsohag360@gmail.com>
2023-10-17 09:36:34 -0700

    fix all error

--- src/components/accounts/LoginForm.tsx
+++ src/components/accounts/LoginForm.tsx
@@ -2,7 +2,6 @@
 import { LoginSchema } from "@/FormValidationSchema/FormValidationSchema"
 import ButtonLarge from "@/components/common/Button/ButtonLarge"
 import { yupResolver } from "@hookform/resolvers/yup"
-import { Button } from "@material-tailwind/react"
 import { signIn } from "next-auth/react"
 import Link from "next/link"
 import { useRouter, useSearchParams } from "next/navigation"
@@ -104,7 +103,6 @@ const LoginForm = () => {
             </div>
             <ButtonLarge
                 handleButton={handleButton} classList="bg-skin-green_shade border border-dark_gray_shade text-skin-white_shade w-full  hover:text-skin-white_shade hover:border-green_shade">Sign in</ButtonLarge>
-            <Button>error</Button>
         </form>
     )
 }
