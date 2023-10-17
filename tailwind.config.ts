a0577102ca7d654a445b363ec59bc46789672663
sohagzayan <gdsohag360@gmail.com>
2023-10-16 21:22:21 -0700

    check again

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,6 +23,8 @@ const config: Config = withMT({
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
